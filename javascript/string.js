d43d69995bcc90351747cd2cb045a725c9b2eb91
Prozpekt-Anchal <shivamthakur@shivams-MacBook-Air.local>
2025-03-13 18:08:08 +0530

    Adding String.js

---
+++ javascript/string.js
@@ -0,0 +1,26 @@
+console.log('Implementing String');
+let a = "STRING";
+console.log(a)
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+
+// Length of array or string
+console.log(a.length); 
+
+//Template Literals
+let name = "Anchal";
+let friend = "Ravensh";
+console.log(`His name is ${name} and his friend is ${friend}.`);
+
+//String properties
+let b = "Hariom";
+console.log(b.toUpperCase());
+console.log(b.toLowerCase());
+
+console.log(b.slice(1,5));
+
+console.log(b.replace("Ha", "22"));
+console.log(a.concat(b + "Anchal"))
